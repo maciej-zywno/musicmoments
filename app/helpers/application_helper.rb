9eb7f36a191cc9f82844b438343494bf96850767
Maciej Zywno <m.zywno@gmail.com>
2015-06-09 23:09:53 +0200

    do not display video title

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
 
   def youtube_link(youtube_vid, start)
-    "https://www.youtube.com/embed/#{youtube_vid}?start=#{start}"
+    "https://www.youtube.com/embed/#{youtube_vid}?start=#{start}&amp;showinfo=0"
   end
 
 end
