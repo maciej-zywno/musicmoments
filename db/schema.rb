d59f9ecb3cbcdf8684e3d4c85ca2be004834ee27
Maciej Zywno <m.zywno@gmail.com>
2015-06-09 17:24:58 +0200

    initial seed

--- db/schema.rb
+++ db/schema.rb
@@ -28,19 +28,19 @@ ActiveRecord::Schema.define(version: 20150609145619) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
-    t.string   "reset_password_token"
+    t.string   "email",                  limit: 255, default: "", null: false
+    t.string   "encrypted_password",     limit: 255, default: "", null: false
+    t.string   "reset_password_token",   limit: 255
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer  "sign_in_count",                      default: 0,  null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.inet     "current_sign_in_ip"
     t.inet     "last_sign_in_ip"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
-    t.string   "name"
+    t.datetime "created_at",                                      null: false
+    t.datetime "updated_at",                                      null: false
+    t.string   "name",                   limit: 255
     t.integer  "role"
   end
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,20 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 user = CreateAdminService.new.call
 puts 'CREATED ADMIN USER: ' << user.email
+
+user = User.create!(email: 'user@gmail.com', name: 'zywy', password: 'password', password_confirmation: 'password')
+
+user.moments.create!(youtube_vid: '6vpXX5BjltM', start: 162, stop: nil, name: nil, description: nil)
+user.moments.create!(youtube_vid: 'UeQKB6s7Ldk', start: 140, stop: nil, name: nil, description: nil)
+user.moments.create!(youtube_vid: '2A2J9yxpSjI', start: 115, stop: nil, name: nil, description: nil)
+user.moments.create!(youtube_vid: 'CRBsGtmNXSw', start: 126, stop: nil, name: nil, description: nil)
+user.moments.create!(youtube_vid: 'UVUiM1lleaA', start: 43,  stop: nil, name: nil, description: nil)
+user.moments.create!(youtube_vid: 'F3W_alUuFkA', start: 350, stop: nil, name: nil, description: nil)
+user.moments.create!(youtube_vid: 'NgU5qZi2TOs', start: 15,  stop: nil, name: nil, description: nil)
+user.moments.create!(youtube_vid: 'G7Q8Ual3coM', start: 80,  stop: nil, name: nil, description: nil)
+user.moments.create!(youtube_vid: 'U6fZG2Runso', start: 235, stop: nil, name: nil, description: nil)
+user.moments.create!(youtube_vid: 'uEh83c1wkMA', start: 52,  stop: nil, name: nil, description: nil)
+user.moments.create!(youtube_vid: 'lyXYwmgItQw', start: 100, stop: nil, name: nil, description: nil)
+user.moments.create!(youtube_vid: '9esWG6A6g-k', start: 81,  stop: nil, name: nil, description: nil)
+user.moments.create!(youtube_vid: 'GekMtAxxYjI', start: 95,  stop: nil, name: nil, description: nil) # drums
+user.moments.create!(youtube_vid: 'WqEOvEoCV3w', start: 4,   stop: nil, name: nil, description: nil) # bass intro
